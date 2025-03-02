93140082491a782b46257e341b45e4cb1c87653c
birddevelper <mostafa_shaeri_tj@yahoo.com>
2025-03-02 23:08:57 +0200

    feat: add map_xml_to_dto

--- dequest/utils.py
+++ dequest/utils.py
@@ -5,6 +5,9 @@ import json
 import logging
 import threading
 from typing import Any, Optional, TypeVar, get_type_hints
+from xml.etree.ElementTree import Element
+
+from defusedxml import ElementTree
 
 T = TypeVar("T")  # Generic Type for DTO
 
@@ -69,3 +72,38 @@ def get_logger() -> logging.Logger:
     logger.addHandler(logging.NullHandler())
 
     return logger
+
+
+def map_xml_to_dto(dto_class: type[T], xml_data: str) -> T | list[T]:
+    root = ElementTree.fromstring(xml_data)
+
+    # If multiple elements exist, return a list
+    if len(root) > 1 and all(child.tag == root[0].tag for child in root):
+        return [_parse_element(dto_class, child) for child in root]
+
+    return _parse_element(dto_class, root)
+
+
+def _parse_element(dto_class: type[T], element: Element) -> T:
+    dto_fields = get_type_hints(dto_class).keys()
+    init_params = inspect.signature(dto_class).parameters
+
+    mapped_data = {}
+    for key in dto_fields:
+        if key in element.attrib:
+            mapped_data[key] = element.attrib[key]
+        else:
+            child = element.find(key)
+            if child is not None:
+                field_annotation = get_type_hints(dto_class)[key]
+
+                # Check if the field is a nested DTO
+                if isinstance(field_annotation, type) and hasattr(field_annotation, "__annotations__"):
+                    mapped_data[key] = _parse_element(field_annotation, child)
+                else:
+                    mapped_data[key] = child.text
+
+    # Filter out attributes that are not in the constructor parameters
+    init_data = {k: v for k, v in mapped_data.items() if k in init_params}
+
+    return dto_class(**init_data)

2c2ac443ee11d55fe2c214020c4855a1cc397af7
birddevelper <mostafa_shaeri_tj@yahoo.com>
2025-04-13 20:20:02 +0300

    feat: add .config() method

--- dequest/config.py
+++ dequest/config.py
@@ -1,3 +1,6 @@
+from enum import StrEnum, auto
+
+
 class DequestConfig:
     CACHE_PROVIDER = "in_memory"  # Options: "in_memory", "redis", "database"
 
@@ -7,3 +10,15 @@ class DequestConfig:
     REDIS_DB = 0
     REDIS_PASSWORD = None
     REDIS_SSL = False
+    PROFILES = {}
+
+    @classmethod
+    def config(cls, **kwargs):
+        for key, value in kwargs.items():
+            setattr(cls, key.upper(), value)
+
+
+class CacheType(StrEnum):
+    IN_MEMORY = auto()
+    REDIS = auto()
+    DJANGO = auto()

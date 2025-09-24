080c6c0acfec9763fad217792d9957569f1c243a
Bird <mostafa_shaeri_tj@yahoo.com>
2025-09-24 18:28:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with Path.open("README.md") as readme_file:
 
 setup_args = {
     "name": "dequest",
-    "version": "0.4.0",
+    "version": "0.4.1",
     "description": "Declarative rest client",
     "long_description_content_type": "text/markdown",
     "long_description": README,
